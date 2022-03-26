f5c35b69f082fead676598a9060bbf8e938d0f35
Johnathan <jocuog@gmail.com>
2022-03-25 19:51:00 -0600

    commit tree b61549e9363b877d4873a57bb2f7f85a25b1e2a4

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Write your solution in this file!
+
+var customerName ='bob'
+
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer () {
+    return  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer () {
+     bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'steve'
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = "tim"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var customerName = 'bob'
+// function upperCaseCustomerName() {
+//     customerName = customerName.toUpperCase();
+// }
+// function setBestCustomer() {
+//     bestCustomer = 'not bob';
+// }
+// function overwriteBestCustomer() {
+//     bestCustomer = 'maybe bob'
+// }
+// const leastFavoriteCustomer = 'Jim'
+// function changeLeastFavoriteCustomer() {
+//     leastFavoriteCustomer = 'tim'
+// } 
